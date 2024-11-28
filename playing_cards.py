221531c5f2b5d5087c26a6fe48a5dfad0803e924
JestZBJ <zbj4acajr@gmail.com>
2024-11-28 09:27:35 -0800

    Update playing_cards.py
    Added 31 to the functional games.

--- playing_cards.py
+++ playing_cards.py
@@ -1,7 +1,3 @@
-# create a Computer object that can have it's own hand and go through a series of ifs to decide what to do
-# deal - deals one card
-# deal_blackjack_hand - deals a hand for blackjack alternating from User to Comp
-
 import random
 import sys
 import time
@@ -12,6 +8,7 @@ class Player:
     def __init__(self, hand):
         self.hand = hand
         self.handValue = 0
+        self.knock = False
 
 def new_standard_deck():
     deck = []
@@ -276,9 +273,262 @@ def play_crazy_eights():
     pass
 
 def play_31():
-    pass
+    deck = new_standard_deck()
+    ans = ""
 
-print("Usually, I would ask what you want to play, but right now there's only one option.  Blackjack.")
-print("It's also still in testing, soooo...  Anyways, here you go.\n")
+    random.shuffle(deck)
 
-play_blackjack()
+    print("How many players?  Please note that the standard 52 card deck can run out of cards and crash the game.")
+    numPlayers = int(input())
+    players = []
+    knock = []
+
+    for p in range(numPlayers):
+        user = Player([])
+        players.append(user)
+
+    for i in range(3):
+        for j in players:
+            deal(j, deck)
+    
+    for q in players:
+        suitVal = [0, 0, 0, 0]
+
+        for a in q.hand:
+            cardVal = 0
+
+            if (a[0] == "2"):
+                cardVal += 2
+            if (a[0] == "3"):
+                cardVal += 3
+            if (a[0] == "4"):
+                cardVal += 4
+            if (a[0] == "5"):
+                cardVal += 5
+            if (a[0] == "6"):
+                cardVal += 6
+            if (a[0] == "7"):
+                cardVal += 7
+            if (a[0] == "8"):
+                cardVal += 8
+            if (a[0] == "9"):
+                cardVal += 9
+            if (a[0] == "1" or a[0] == "J" or a[0] == "Q" or a[0] == "K"):
+                cardVal += 10
+            if (a[0] == "A"):
+                cardVal += 11
+            
+            if (a[-1] == "C"):
+                suitVal[0] += cardVal
+            if (a[-1] == "H"):
+                suitVal[1] += cardVal
+            if (a[-1] == "S"):
+                suitVal[2] += cardVal
+            if (a[-1] == "D"):
+                suitVal[3] += cardVal
+        
+        q.handValue = max(suitVal)
+    
+    discardTop = "EMPTY"
+    discardPile = []
+    run = True
+    
+    while (run == True):
+        playerNum = 1
+
+        for b in players:
+            if (b.knock == True):
+                run = False
+                break
+
+            os.system("cls")
+
+            while (ans.lower() != "continue" and ans.lower() != "/*-"):
+                print("Player", str(playerNum), "'s turn.  Pass device to Player " + str(playerNum) + " and type *Continue* if ready to play.  (Alternate Controls:  /*- Continue)")
+                ans = str(input())
+            
+            print("\nPlayer " + str(playerNum) + "'s hand:")
+            print(b.hand, b.handValue)
+            print("\nCard in discard pile: ", discardTop, discardPile)
+            while (ans.lower() != "deck draw" and ans.lower() != "discard draw" and ans.lower() != "knock" and ans.lower() != "1" and ans.lower() != "2" and ans.lower() != "3"):
+                print("\nDeck Draw, Discard Draw, or Knock?  (Alternate Controls:  1 Deck Draw, 2 Discard Draw, 3 Knock)")
+                ans = str(input())
+
+            if (ans.lower() == "deck draw" or ans.lower() == "1"):
+                deal(b, deck)
+
+                print("\nPlayer", str(playerNum), "'s hand:")
+                print(b.hand)
+
+                if (b.hand[0][0] == b.hand[1][0] and b.hand[0][0] == b.hand[2][0] and b.hand[0][0] == b.hand[3][0]):
+                    print("\nYou have collected all four", b.hand[0][0], "'s.  Player", str(playerNum), "wins!")
+                    sys.exit()
+                
+                print("\nWhich card would you like to dicard.  (CASE SENSITIVE: TYPE THE CARD VALUE AS YOU SEE IT.)")
+                ans = str(input())
+
+                b.hand.remove(ans)
+                if (discardTop == "EMPTY"):
+                    pass
+                else:
+                    discardPile.insert(0, discardTop)
+                discardTop = ans
+                
+                suitVal = [0, 0, 0, 0]
+                for s in b.hand:
+                    cardVal = 0
+
+                    if (s[0] == "2"):
+                        cardVal += 2
+                    if (s[0] == "3"):
+                        cardVal += 3
+                    if (s[0] == "4"):
+                        cardVal += 4
+                    if (s[0] == "5"):
+                        cardVal += 5
+                    if (s[0] == "6"):
+                        cardVal += 6
+                    if (s[0] == "7"):
+                        cardVal += 7
+                    if (s[0] == "8"):
+                        cardVal += 8
+                    if (s[0] == "9"):
+                        cardVal += 9
+                    if (s[0] == "1" or s[0] == "J" or s[0] == "Q" or s[0] == "K"):
+                        cardVal += 10
+                    if (s[0] == "A"):
+                        cardVal += 11
+                    
+                    if (s[-1] == "C"):
+                        suitVal[0] += cardVal
+                    if (s[-1] == "H"):
+                        suitVal[1] += cardVal
+                    if (s[-1] == "S"):
+                        suitVal[2] += cardVal
+                    if (s[-1] == "D"):
+                        suitVal[3] += cardVal
+                
+                b.handValue = max(suitVal)
+                print()
+                print(b.hand, b.handValue)
+            elif (ans.lower() == "discard draw" or ans.lower() == "2"):
+                if (discardTop == "EMPTY"):
+                    print("\nNo card in discrdpile to draw.  Drawing from deck instead.")
+
+                    deal(b, deck)
+                else:
+                    b.hand.append(discardTop)
+
+                print("\nPlayer", str(playerNum), "'s hand:")
+                print(b.hand)
+
+                if (b.hand[0][0] == b.hand[1][0] and b.hand[0][0] == b.hand[2][0] and b.hand[0][0] == b.hand[3][0]):
+                    print("\nYou have collected all four", b.hand[0][0], "'s.  Player", str(playerNum), "wins!")
+                    sys.exit()
+                if ("8C" in b.hand and "8S" in b.hand and "AC" in b.hand and "AS" in b.hand):
+                    print("\nYou have collected a Deadman's Hand.  Player", str(playerNum), "wins!")
+                    sys.exit()
+
+                print("\nWhich card would you like to dicard.  (CASE SENSITIVE: TYPE THE CARD VALUE AS YOU SEE IT.)")
+                ans = str(input())
+
+                b.hand.remove(ans)
+                if (discardTop == "EMPTY"):
+                    pass
+                else:
+                    discardPile.insert(0, ("[" + discardTop + " P" + str(playerNum) + " picked up]"))
+                discardTop = ans
+                
+                suitVal = [0, 0, 0, 0]
+                for d in b.hand:
+                    cardVal = 0
+
+                    if (d[0] == "2"):
+                        cardVal += 2
+                    if (d[0] == "3"):
+                        cardVal += 3
+                    if (d[0] == "4"):
+                        cardVal += 4
+                    if (d[0] == "5"):
+                        cardVal += 5
+                    if (d[0] == "6"):
+                        cardVal += 6
+                    if (d[0] == "7"):
+                        cardVal += 7
+                    if (d[0] == "8"):
+                        cardVal += 8
+                    if (d[0] == "9"):
+                        cardVal += 9
+                    if (d[0] == "1" or d[0] == "J" or d[0] == "Q" or d[0] == "K"):
+                        cardVal += 10
+                    if (d[0] == "A"):
+                        cardVal += 11
+                    
+                    if (d[-1] == "C"):
+                        suitVal[0] += cardVal
+                    if (d[-1] == "H"):
+                        suitVal[1] += cardVal
+                    if (d[-1] == "S"):
+                        suitVal[2] += cardVal
+                    if (d[-1] == "D"):
+                        suitVal[3] += cardVal
+                
+                b.handValue = max(suitVal)
+                print()
+                print(b.hand, b.handValue)
+            elif (ans.lower() == "knock" or ans.lower() == "3"):
+                b.knock = True
+                print("\nPlease anounce to all players that you have knocked.")
+            
+            if (b.handValue == 31):
+                print("\nYour hand has reached 31.  Player", str(playerNum), "wins!")
+                sys.exit()
+            if (b.hand[0][0] == "A" and b.hand[1][0] == "A" and b.hand[2][0] == "A"):
+                print("\nYou have collected three A's.  Player", str(playerNum), "wins!")
+                sys.exit()
+            if (b.handValue > 31):
+                print("\nCongratulations.  You broke the game and somehow went bust.")
+                sys.exit()
+            
+            while (ans.lower() != "clear" and ans.lower() != "-*/"):
+                print("\nType *Clear* when you are ready to end your turn.  (Alternate Controls:  -*/ Clear)")
+                ans = str(input())
+            
+            playerNum += 1
+    
+    playerNum = 0
+    winHand = 0
+    winners = []
+
+    for u in players:
+        playerNum += 1
+
+        if u.handValue > winHand:
+            winHand = u.handValue
+            winners = [playerNum]
+        elif u.handValue == winHand:
+            winners.append(playerNum)
+
+    os.system("cls")
+
+    print("End of game.  Hightest hand:", winHand)
+    if (len(winners) == 1):
+        print("Player", winners[0], "wins!")
+    else:
+        numWinners = len(winners)
+        print("Players", end=" ")
+
+        for r in range(numWinners):
+            print(winners[r], "and", end="")
+        
+        print("wins!")
+
+print("Hey!  I've got more than one game now!  I can offer you Blackjack or 31.")
+print("So, which would you like to play?")
+
+choice = str(input())
+
+if (choice.lower() == "blackjack"):
+    play_blackjack()
+if (choice.lower() == "31"):
+    play_31()
